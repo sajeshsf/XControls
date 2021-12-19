e3572b0a05746e5361762ae47a3436f151e6513f
Sajesh S F <sf.sajesh@gmail.com>
2021-12-19 21:54:48 +0530

    added new project

---
+++ XControls.Loader/Loader.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Forms;
+using XMessageBox;
+
+namespace XControls.Loader
+{
+    public class Loader
+    {
+        private LoaderWindow Window { get; set; }
+        private Loader()
+        {
+            Window = new LoaderWindow();
+        }
+        public void ShowLoader() => Window.Show();
+        public void Hide()
+        {
+            Window.Close();
+            Cursor.Current = Cursors.Default;// Set cursor as default 
+        }
+        public static Loader Show()
+        {
+            Cursor.Current = Cursors.WaitCursor;// Set cursor as hourglass
+            var loader = new Loader();
+            if (System.Windows.Application.Current?.MainWindow?.IsLoaded == true)
+            {
+                loader.Window.Owner = System.Windows.Application.Current.MainWindow;
+            }
+            loader.ShowLoader();
+            return loader;
+        }
+    }
+}

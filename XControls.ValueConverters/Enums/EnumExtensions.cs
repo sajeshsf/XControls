23155cb2f63f8709e2a65fba21ed29a618145298
Sajesh S F <sf.sajesh@gmail.com>
2021-12-26 00:21:48 +0530

    added new class

---
+++ XControls.ValueConverters/Enums/EnumExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace XControls.ValueConverters.Enums
+{
+    public static class EnumExtensions
+    {
+        public static object GetDescription(object value) => DescriptionConverter.GetDescription(value).ToString();
+    }
+}
